ba6b8c6e4e39b80f0c2297da0963a468c491e899
Romulo Ciro <romulo.ciro@hotmail.com>
2023-06-28 09:45:46 -0300

    feat: add route webhook

--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
 const express = require('express');
-const app = express();
 const paymentController = require('./controllers/paymentController');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
 
 
 app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(bodyParser.json());
 
 app.get('/', paymentController.getPayment);
 app.get('/qrcode/:id', paymentController.getQRCode);
+
+app.post('/webhook(/pix)?', (req, res) => {
+    console.log(req.body)
+    res.send(200)
+});
 
 // Adicione outras rotas e controladores conforme necessário
 
